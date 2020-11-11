98540c53661f62e2465165fb262507345d681a72
TomasdeA <tdeaguirre@fi.uba.ar>
2020-11-11 13:44:16 -0300

    Tomi: edición busqueda de raiz

--- BusquedaRaices.py
+++ BusquedaRaices.py
@@ -4,8 +4,8 @@ import sys
 MSG_ERROR_COTAS="Error de selección de cotas"
 
 min_error = float(input("inserte el error "))
-cota_mayor = float(input("inserte una cota superior "))
-cota_menor = float(input("inserte una cota inferior "))
+lim_sup = float(input("inserte un límite superior "))
+lim_inf = float(input("inserte un límite inferior "))
 
 
 def f1(x): return 0.25*x**2 - np.sin(x)
@@ -25,52 +25,41 @@ def round_up(n, decimals):
     multiplier = 10 ** decimals
     return np.ceil(n * multiplier) / multiplier
 
-def MagnitudError(min_error):
-    #solo para error <= 1
-    num_dig_error=0
-    aux=np.trunc(min_error)
-    while aux==0:
-        num_dig_error+=1
-        aux=np.trunc(min_error*10**num_dig_error)
-    return num_dig_error
-
 def pn(a,b): return (b+a)/2
 
-def RaizBinomial(funcion,cota_menor,cota_mayor,min_error):
+def RaizBinomial(funcion,lim_inf,lim_sup,min_error):
   
-    if funcion(cota_menor)*funcion(cota_mayor) >=0:  
+    if funcion(lim_inf)*funcion(lim_sup) >=0:  
         print(MSG_ERROR_COTAS) 
         sys.exit(1)
       
-    print("\nIteraciones del método de Bisección\n")
-    n=np.log2((cota_mayor-cota_menor)/min_error)
-     
-    p=pn(cota_menor,cota_mayor )
-    ep=cota_mayor-p
+    print("Iteraciones del método de Bisección\n")
+    n=int(np.log2((lim_sup-lim_inf)/min_error))
+    print("cantidad de iteraciones para hacer", n)
+
+    i=0
+    b=lim_sup
+    a=lim_inf
     
-    i=1
-    print(i,"°  ","{0} +- {1}".format(p,ep))
-    b=cota_mayor
-    a=cota_menor
-   
-    while i<(n):
+    while i < (n):
+
+        p = pn(a,b)
+        ep = abs(b-p)
+        i+=1
+        print(i,"°  ","{0} +- {1}".format(p,ep))
+
         if (funcion(p)*funcion(b)) < 0:
-            a=p
-            p=pn(a,b)
-            ep=ep/2
-            print(i+1,"°  ","{0} +- {1}".format(p,ep))
-            i+=1
+            a = p
         elif (funcion(p)*funcion(a) < 0):
-            b=p
-            p=pn(a,b)
-            ep=ep/2
-            print(i+1,"°  ","{0} +- {1}".format(p,ep))
-            i+=1
-             
-    print("\nSe hicieron",i,"iteraciones con el método de bisección\n")
+            b = p
+        else :
+            print("Hay algo que no esta bien")
+            break
+            
+    print("Se hicieron",i,"iteraciones con el método de bisección\n")
     
-    num_dig_error=MagnitudError(min_error)   
-    ep=round_up(ep,num_dig_error)
+    num_dig_error = int(np.ceil(abs(np.log10(min_error))))   
+    ep=round_up(abs(ep),num_dig_error)
     
     return p,ep
 
@@ -79,18 +68,18 @@ def RaizBinomial(funcion,cota_menor,cota_mayor,min_error):
 --------------------------------------------------------------------------------
 """
 
-def RaizSecante(funcion,cota_menor,cota_mayor,min_error): 
+def RaizSecante(funcion,lim_inf,lim_sup,min_error): 
     
-    (semilla,cota) = RaizBinomial(funcion,cota_menor,cota_mayor,0.02)
+    (semilla,cota) = RaizBinomial(funcion,lim_inf,lim_sup,0.02)
     
-    cota_mayor = semilla + cota
-    cota_menor = semilla - cota
+    lim_sup = semilla + cota
+    lim_inf = semilla - cota
     p = semilla
     
     print("Iteraciones del método de la secante\n")
     
-    #las varibles cota_mayor,cota_menor y p están para luego analizar bien las condiciones necesarias para el uso del Método de la secante
-    p_n_2 = cota_mayor 
+    #las varibles lim_sup,lim_inf y p están para luego analizar bien las condiciones necesarias para el uso del Método de la secante
+    p_n_2 = lim_sup 
     p_n_1 = p
     aux = p_n_1 - p_n_2
     
@@ -107,7 +96,7 @@ def RaizSecante(funcion,cota_menor,cota_mayor,min_error):
     
     print("\nSe hicieron",i,"iteraciones con el método de la secante\n")
     
-    num_dig_error=MagnitudError(min_error)   
+    num_dig_error = int(np.ceil(abs(np.log10(min_error))))   
     raiz=np.round(p, num_dig_error) 
     error=round_up(abs(aux),num_dig_error)
     
@@ -120,11 +109,11 @@ def RaizSecante(funcion,cota_menor,cota_mayor,min_error):
 
 def g(x) : return (funcion(x) + x)
 
-def RaizPF(funcion,cota_menor,cota_mayor,min_error): 
+def RaizPF(funcion,lim_inf,lim_sup,min_error): 
     
-    (semilla,cota) = RaizBinomial(funcion,cota_menor,cota_mayor,0.02)
-    cota_mayor = semilla + cota
-    cota_menor = semilla - cota
+    (semilla,cota) = RaizBinomial(funcion,lim_inf,lim_sup,0.02)
+    lim_sup = semilla + cota
+    lim_inf = semilla - cota
     p = semilla
     
     print("Iteraciones del método del Punto fijo\n")
@@ -140,12 +129,12 @@ def RaizPF(funcion,cota_menor,cota_mayor,min_error):
       p_next = g(p)
       aux = (p_next - p)
       p = p_next
-      print(i+1,"°  ",p_next)
       i+=1
+      print(i,"°  ",p_next)
     
     print("\nSe hicieron",i,"iteraciones con el método del Punto Fijo\n")
     
-    num_dig_error=MagnitudError(min_error)   
+    num_dig_error = int(np.ceil(abs(np.log10(min_error))))   
     raiz=np.round(p_next, num_dig_error) 
     error=round_up(abs(aux),num_dig_error)
     return raiz,error
@@ -156,15 +145,12 @@ def RaizPF(funcion,cota_menor,cota_mayor,min_error):
 
 funcion = f4
 
-#(raiz,error)=RaizBinomial(funcion, cota_menor, cota_mayor, min_error)
+#(raiz,error)=RaizBinomial(funcion, lim_inf, lim_sup, min_error)
 
-(raiz,error)=RaizSecante(funcion, cota_menor, cota_mayor, min_error)
+(raiz,error)=RaizSecante(funcion, lim_inf, lim_sup, min_error)
 
-#(raiz,error)=RaizPF(funcion, cota_menor, cota_mayor, min_error)
+#(raiz,error)=RaizPF(funcion, lim_inf, lim_sup, min_error)
 
 print("El valor de la raiz es:{0} +- {1}".format(raiz,error))
 print("El valor de la función en la aproximación a la raiz es: ",funcion(raiz))
-
-
-
 
