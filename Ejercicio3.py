7490ea0e9bab20ccbbd5662b41919812cdd0d13d
TomasdeA <tdeaguirre@fi.uba.ar>
2020-11-18 09:13:28 -0300

    santi cagon

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -11,12 +11,11 @@ ERROR_BISECCION = 0.02
 
 
 #suma de los ultimos digitos de los padrones de los estudiantes del grupo
-aux=0
+s=0
 n=0
 for i in padrones:
-    aux += (int(repr(i)[-1]))
+    s += (int(repr(i)[-1]))
     n +=1
-s=aux
 porcentaje = s / (n * 9.5)
 
 def V(x):  return np.pi * x** 2 *(3* Radio - x) / 3
