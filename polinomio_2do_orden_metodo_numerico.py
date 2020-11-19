dbe5b7bcd6ef86f1e28482911a0b8322daff2df6
TomasdeA <tdeaguirre@fi.uba.ar>
2020-11-19 10:32:13 -0300

    funciona el ej2 pero NRmodif converge lento

--- polinomio_2do_orden_metodo_numerico.py
+++ polinomio_2do_orden_metodo_numerico.py
@@ -25,12 +25,12 @@ if radicando < 0:
 
 
 #busqueda de pico de la función
-(matriz_sem_pico, it_pico) = br.RaizBiseccion(f_prima, lim_inf, lim_sup, ERROR_BISECCION)
+#(matriz_sem_pico, it_pico) = br.RaizBiseccion(f_prima, lim_inf, lim_sup, ERROR_BISECCION)
 
-sem_pico = (matriz_sem_pico[it_pico - 1])[1]
-cota_sem_pico = (matriz_sem_pico[it_pico - 1])[2]
+#sem_pico = (matriz_sem_pico[it_pico - 1])[1]
+#cota_sem_pico = (matriz_sem_pico[it_pico - 1])[2]
 
-(matriz_pico, it_pico) = br.RaizNR(f_prima, f_segunda, min_error, sem_pico, cota_sem_pico)
+(matriz_pico, it_pico) = br.RaizNR(f_prima, f_segunda, min_error, 0.02, 0.01)
 
 pico = (matriz_pico[it_pico - 1])[1]
 error_pico = (matriz_pico[it_pico - 1])[2]
@@ -41,7 +41,7 @@ error_pico = (matriz_pico[it_pico - 1])[2]
 sem_1 = (matriz_sem_1[it_sem_1 - 1])[1]
 cota_sem_1 = (matriz_sem_1[it_sem_1 - 1])[2]
 
-(matriz_raiz_1, it_raiz_1) = br.RaizNR(f, f_prima, min_error, sem_1, cota_sem_1)
+(matriz_raiz_1, it_raiz_1) = br.RaizNRmodificado(f, f_prima, f_segunda, min_error, sem_1, cota_sem_1)
 
 #segunda raiz
 (matriz_sem_2, it_sem_2) = br.RaizBiseccion(f, pico, lim_sup, ERROR_BISECCION)
@@ -49,8 +49,7 @@ cota_sem_1 = (matriz_sem_1[it_sem_1 - 1])[2]
 sem_2 = (matriz_sem_2[it_sem_2 - 1])[1]
 cota_sem_2 = (matriz_sem_2[it_sem_2 - 1])[2]
 
-(matriz_raiz_2, it_raiz_2) = br.RaizNR(f, f_prima, min_error, sem_2, cota_sem_2)
-
+(matriz_raiz_2, it_raiz_2) = br.RaizNRmodificado(f, f_prima, f_segunda,min_error, sem_2, cota_sem_2)
 raiz_1 = format((matriz_raiz_1[it_raiz_1 - 1])[1], '.7g')
 raiz_2 = format((matriz_raiz_2[it_raiz_2 - 1])[1], '.7g')
 error_raiz_1 = format((matriz_raiz_1[it_raiz_1 - 1])[2], '.7g')
